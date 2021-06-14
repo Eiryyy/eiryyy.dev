d1c264fc7b4cc59909986486b94b1560762d650e
Eiryyy <xxx.eiryklav@gmail.com>
2021-06-15 00:40:28 +0900

    Use Next.js

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>eiryyy.dev</title>
+        <meta name="description" content="eiryyy.dev" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <h1>eiryyy.dev</h1>
+      </header>
+    </>
+  );
+};
+
+export default Home;
